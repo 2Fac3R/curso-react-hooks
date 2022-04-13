d39733ed51cef74d65d489ce6738bc7cb593c498
Gustavo Navarro <genr1818@gmail.com>
2022-04-13 17:15:25 -0500

    useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
   const [theme, setTheme] = useState(false);
+  const color = useContext(ThemeContext);
 
   return (
     <div className='Header'>
-      <h1>Header</h1>
+      <h1 style={{ color }}>React Hooks</h1>
       <p>Theme: {theme ? 'Light Theme' : 'Dark Theme'}</p>
       <button
         onClick={() => setTheme(false)}
